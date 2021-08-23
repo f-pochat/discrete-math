0386850dd8d8ca9af04dea7dfc9ea473b950660a
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2021-08-22 21:47:48 -0300

    Tp1 terminado

--- src/tp1/Tp1Impl.java
+++ src/tp1/Tp1Impl.java
@@ -2,13 +2,20 @@ package tp1;
 
 import graph.Graph;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO: implement
 public class Tp1Impl<T> implements Tp1<T> {
     @Override
     public void exercise_a(Graph<T> graph) {
-        throw new UnsupportedOperationException("TODO");
+        for (int i = 0; i < graph.order(); i++) {
+            System.out.println("Vertex: " + graph.getVertexes().get(i));
+            String str = "Edges: ";
+            for (int j = 0; j < graph.getAdjacencyList(graph.getVertexes().get(i)).size(); j++) {
+                str += " -> " + graph.getAdjacencyList(graph.getVertexes().get(i)).get(j);
+            }
+        }
     }
 
     @Override
@@ -18,7 +25,13 @@ public class Tp1Impl<T> implements Tp1<T> {
 
     @Override
     public List<T> exercise_c(Graph<T> graph) {
-        throw new UnsupportedOperationException("TODO");
+        List<T> lst = new ArrayList<>();
+        for (int i = 0; i < graph.order(); i++) {
+            if (graph.getAdjacencyList(graph.getVertexes().get(i)).contains(graph.getVertexes().get(i))){
+               lst.add(graph.getVertexes().get(i));
+            }
+        }
+        return lst;
     }
 
     @Override
@@ -33,12 +46,26 @@ public class Tp1Impl<T> implements Tp1<T> {
 
     @Override
     public List<T> exercise_f(Graph<T> graph) {
-        throw new UnsupportedOperationException("TODO");
+        List<T> lst = new ArrayList<>();
+        for (int i = 0; i < graph.order(); i++) {
+            if (graph.getAdjacencyList(graph.getVertexes().get(i)).isEmpty()){
+                lst.add(graph.getVertexes().get(i));
+            }
+        }
+        return lst;
     }
 
     @Override
     public Graph<T> exercise_g(Graph<T> graph) {
-        throw new UnsupportedOperationException("TODO");
+        List<T> lst_loops = exercise_c(graph);
+        List<T> lst_isolated = exercise_f(graph);
+        for (int i = 0; i < lst_loops.size(); i++) {
+            graph.removeEdge(lst_loops.get(i), lst_loops.get(i));
+        }
+        for (int i = 0; i < lst_isolated.size(); i++) {
+            graph.removeVertex(lst_isolated.get(i));
+        }
+        return graph;
     }
 
     @Override
